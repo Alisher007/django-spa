83c938a920002ad1bdfa84f1aac2f83bf2769f32
Alisher007 <alisherx07@gmail.com>
2020-01-11 12:32:58 +0400

    settings changed

--- spa4/settings.py
+++ spa4/settings.py
@@ -79,15 +79,8 @@ WSGI_APPLICATION = 'spa4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-# DATABASE_URL = os.environ.get('DATABASE_URL')
+
 DATABASES = {}
-# DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
